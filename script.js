f7fa5557d2318db543a7b8edd8866086018f3101
UmeshMula <136836947+UmeshMula@users.noreply.github.com>
2023-07-12 23:18:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-//your code here
+() => {
+  const baseUrl = "http://example.com"; // Replace with your base URL
+  
+  cy.visit(baseUrl + "/main.html");
+  
+  cy.get('nav').within(() => {
+    cy.get('ul').within(() => {
+      cy.get('li').should('have.length', 3);
+      cy.get('a').should('have.length', 3);
+    });
+  });
+}
