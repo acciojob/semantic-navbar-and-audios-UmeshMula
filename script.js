bdb0cf08e82e6a4956e400fdbd643a3d80ea6ed7
UmeshMula <136836947+UmeshMula@users.noreply.github.com>
2023-07-12 23:22:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 () => {
-  const baseUrl = "http://example.com"; // Replace with your base URL
-  
+  const baseUrl = "http://commondatastorage.googleapis.com/codeskulptor-demos/DDR_assets/Kangaroo_MusiQue_-_The_Neverwritten_Role_Playing_Game.mp3"; // Replace with your base URL
+  const baseUrl = "http://commondatastorage.googleapis.com/codeskulptor-assets/Epoq-Lepidoptera.ogg";
+	const baseUrl = "http://codeskulptor-demos.commondatastorage.googleapis.com/pang/paza-moduless.mp3";
   cy.visit(baseUrl + "/main.html");
   
   cy.get('nav').within(() => {
